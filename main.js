d6873d06c581cf926d8c7da4ffa5d77536356bfc
Gabriel Lugo Méndez <gabriel.lugo.mendez@gmail.com>
2022-11-24 16:05:13 +0100

    startover

--- main.js
+++ main.js
@@ -3,3 +3,14 @@ window.addEventListener('DOMContentLoaded', main);
 function main(){
     
 }
+
+
+// mediaquerys för JS
+// window.addEventListener('resize', updateLayout);
+
+// function updateLayout(event) {
+//     console.log('RESIZE', window.innerWidth);
+//     if (window.innerWidth < 500) {
+//         //todo...
+//     }
+// }
