341399f32a2796e6592495c7d010d3ab4cf1fb21
Gabriel Lugo Méndez <gabriel.lugo.mendez@gmail.com>
2022-11-25 09:11:41 +0100

    started over

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 window.addEventListener('DOMContentLoaded', main);
 
 function main(){
+    addEventListeners();
     
+}
+
+function addEventListeners() {
+    const burgerMenuButton = document.getElementById('burger-menu');
+    burgerMenuButton.onclick = toggleMenu;
+}
+
+function toggleMenu() {
+    const header = document.querySelector('header');
+    header.classList.toggle('open');
+
+    const lowerSection = document.getElementById('lower-section');
+    lowerSection.classList.toggle('flex')
 }
 
 
