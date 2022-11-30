20ef55c1cf4181f34ebf9c464056603d96d88970
Gabriel Lugo Méndez <gabriel.lugo.mendez@gmail.com>
2022-11-30 15:39:48 +0100

    added more styling

--- main.js
+++ main.js
@@ -5,12 +5,17 @@ function main() {
 }
 
 function addEventListeners() {
-    const dropdownMenuButton = document.getElementById('dropdown-menu');
+    const dropdownMenuButton = document.getElementById('toggle-box');
     dropdownMenuButton.addEventListener('click', toggleMenu);
     
 }
 
 function toggleMenu() {
     const menu = document.querySelector('.navbar');
+    const menuButton = document.getElementById('menu-button');
+    const closeButton = document.getElementById('close-button');
+
     menu.classList.toggle('open');
+    menuButton.classList.toggle('hide');
+    closeButton.classList.toggle('show');
 }
